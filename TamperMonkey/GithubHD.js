16d520c50a718f6fa974aec8f4ba60ab8be39fae
Francis <fcorriveau@omnimed.com>
2018-09-12 11:13:49 -0400

    Update GithubHD.js
    Genre de fix pour les nouveaux styles de github.

--- TamperMonkey/GithubHD.js
+++ TamperMonkey/GithubHD.js
@@ -1,36 +1,39 @@
 // ==UserScript==
 // @name       GitHub HD Resolution
-// @namespace  
-// @version    2016-05-02
-// @description  Resize main container of GitHub to take advantage of HD Resolution Screen.
+// @namespace
+// @version    2018.09-1
+// @description  Welcome to the year 2000s, HD Resolution is here !
 // @match      https://github.com/*
-// @copyright  2014+, Omnimed.com
-// @require        https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @copyright  2018+, Omnimed.com
+// @require        https://code.jquery.com/jquery-3.3.1.min.js
 // ==/UserScript==
 
-var $ = unsafeWindow.jQuery;
+(function() {
+    'use strict';
 
-$('.container').css('width', '95%');
-$('#js-repo-pjax-container').css('width', '95%');
-$('.js-quote-selection-container').css('width', '87%');
-
-$('.repository-with-sidebar .repository-sidebar').css({
-    'float': 'right',
-    'width': '38px',
-    'overflow': 'hidden'
-});
-
-
-$('.repository-with-sidebar .repository-sidebar span:not(.octicon, .url-box-clippy)').css('display', 'none');
-//$('.repository-content, .context-loader-container').css('width', '97%');
-$('#files .diffstat+.css-truncate-target').removeClass('css-truncate css-truncate-target');
-$('span.css-truncate-target').removeClass('css-truncate css-truncate-target');
-$('.container .discussion-timeline').removeClass('discussion-timeline');
-$('.repo-label').remove();
-
-$(document).bind('DOMSubtreeModified', function() {
+    $('.container-lg').css('max-width', 'none');
     $('.container').css('width', '95%');
-	$('#files .diffstat+.css-truncate-target').removeClass('css-truncate css-truncate-target');
-	$('span.css-truncate-target').removeClass('css-truncate css-truncate-target');
-	$('.container .discussion-timeline').removeClass('discussion-timeline');    
-});
+    $('#js-repo-pjax-container').css('width', '95%');
+    $('.js-quote-selection-container').css('width', '87%');
+
+    $('.repository-with-sidebar .repository-sidebar').css({
+        'float': 'right',
+        'width': '38px',
+        'overflow': 'hidden'
+    });
+
+    $('.repository-with-sidebar .repository-sidebar span:not(.octicon, .url-box-clippy)').css('display', 'none');
+    //$('.repository-content, .context-loader-container').css('width', '97%');
+    $('#files .diffstat+.css-truncate-target').removeClass('css-truncate css-truncate-target');
+    $('span.css-truncate-target').removeClass('css-truncate css-truncate-target');
+    $('.container .discussion-timeline').removeClass('discussion-timeline');
+    $('.repo-label').remove();
+
+    $(document).bind('DOMSubtreeModified', function() {
+        $('.container-lg').css('max-width', 'none');
+        $('.container').css('width', '95%');
+        $('#files .diffstat+.css-truncate-target').removeClass('css-truncate css-truncate-target');
+        $('span.css-truncate-target').removeClass('css-truncate css-truncate-target');
+        $('.container .discussion-timeline').removeClass('discussion-timeline');
+    });
+})();
