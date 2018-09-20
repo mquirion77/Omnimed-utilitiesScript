1d1e3e1d1d9be5d267e667871ccf4e9ef409c72d
Francis <fcorriveau@omnimed.com>
2018-09-20 09:06:48 -0400

    Update GithubHD.js

--- TamperMonkey/GithubHD.js
+++ TamperMonkey/GithubHD.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       GitHub HD Resolution
 // @namespace
-// @version    2018.09-2
+// @version    2018.09-3
 // @description  Welcome to the year 2000s, HD Resolution is here !
 // @match      https://github.com/*
 // @copyright  2018+, Omnimed.com
@@ -13,7 +13,7 @@
 
     var resize = function() {
       $('.container.new-discussion-timeline.experiment-repo-nav').width('98%');
-      $('.discussion-timeline.pull-discussion-timeline.js-pull-discussion-timeline.js-quote-selection-container.js-review-state-classes.is-review-pending').width('80%');
+      $('.discussion-timeline.pull-discussion-timeline.js-pull-discussion-timeline.js-quote-selection-container.js-review-state-classes').width('80%');
       $('.container').width('98%');
       $('.container-lg').width('98%');
       $('.container-lg').css('max-width', '100%');
