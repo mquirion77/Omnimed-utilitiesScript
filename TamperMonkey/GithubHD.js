eba07187aa77ac19a4a10ad5ba38e4217e577bcd
Francis <fcorriveau@omnimed.com>
2018-09-20 09:04:43 -0400

    Update GithubHD.js

--- TamperMonkey/GithubHD.js
+++ TamperMonkey/GithubHD.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       GitHub HD Resolution
 // @namespace
-// @version    2018.09-1
+// @version    2018.09-2
 // @description  Welcome to the year 2000s, HD Resolution is here !
 // @match      https://github.com/*
 // @copyright  2018+, Omnimed.com
@@ -11,35 +11,17 @@
 (function() {
     'use strict';
 
-    $('.container-lg').css('max-width', 'none');
-    $('.container').css('width', '95%');
-    $('#js-repo-pjax-container').css('width', '95%');
-    $('.js-quote-selection-container').css('width', '87%');
-
-    $('.repository-with-sidebar .repository-sidebar').css({
-        'float': 'right',
-        'width': '38px',
-        'overflow': 'hidden'
-    });
-
-    $('.edit-comment-hide').css({'height': '50px'});
-
-    $('.repository-with-sidebar .repository-sidebar span:not(.octicon, .url-box-clippy)').css('display', 'none');
-    //$('.repository-content, .context-loader-container').css('width', '97%');
-    $('#files .diffstat+.css-truncate-target').removeClass('css-truncate css-truncate-target');
-    $('span.css-truncate-target').removeClass('css-truncate css-truncate-target');
-    $('.container .discussion-timeline').removeClass('discussion-timeline');
-    $('.repo-label').remove();
+    var resize = function() {
+      $('.container.new-discussion-timeline.experiment-repo-nav').width('98%');
+      $('.discussion-timeline.pull-discussion-timeline.js-pull-discussion-timeline.js-quote-selection-container.js-review-state-classes.is-review-pending').width('80%');
+      $('.container').width('98%');
+      $('.container-lg').width('98%');
+      $('.container-lg').css('max-width', '100%');
+    };
 
     $(document).bind('DOMSubtreeModified', function() {
-        $('.container-lg').css('max-width', 'none');
-        $('.container').css('width', '95%');
-        $('#files .diffstat+.css-truncate-target').removeClass('css-truncate css-truncate-target');
-        $('span.css-truncate-target').removeClass('css-truncate css-truncate-target');
-        $('.container .discussion-timeline').removeClass('discussion-timeline');
+        resize();
     });
 
-    $("<style type='text/css'> .clearfix::before{ display:none !important;} </style>").appendTo("head");
-    $("<style type='text/css'> .clearfix::after{ display:none !important;} </style>").appendTo("head");
-
+    $(resize());
 })();
