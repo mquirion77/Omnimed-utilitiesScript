5eebbbc5fbad2dbe9fa69d7a794a37d30f4c8237
Gabriel Girard <ggirard@omnimed.com>
2019-05-14 10:10:30 -0400

    Update standupBot.js

--- TamperMonkey/misc/standupBot.js
+++ TamperMonkey/misc/standupBot.js
@@ -1,21 +1,26 @@
 // ==UserScript==
 // @name         Join Meet
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://meet.google.com/*
-// @require      https://code.jquery.com/jquery-2.1.4.min.js
+// @require      http://code.jquery.com/jquery-3.3.1.min.js
 // @require      file:///home/devjava/eventInfo.js
 // @grant        window.close
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 var currentState = 1;
-var botLigthUrl = http://192.168.251.252;
+var botLigthUrl = "";
+var timeout = endDate.getTime() - new Date().getTime();
+
+if (timeout < 0) {
+    timeout = 1;
+}
 
 $(document).ready(function() {
-    console.log(endDate.getTime() - new Date().getTime());
+    console.log(timeout);
     setTimeout(function() {
         if ($('input').length == 0 && $(".e3Duub[role='button']").length > 0) {
             $(".e3Duub[role='button']").click();
@@ -25,42 +30,48 @@ $(document).ready(function() {
     }, 20000);
     setTimeout(function() {
         setInterval(function(){
-            if (parseInt($(".wnPUne.N0PJ8e").innerText) < 2) {
-                GM_xmlhttpRequest({
-                    method: "GET",
-                    url: botLigthUrl + "/off",
-                    onload: function(res) {
-                        window.close();
-                    }
-                });
+            if (parseInt($(".wnPUne.N0PJ8e")[0].innerText) < 2) {
+                if (botLigthUrl !== "") {
+                    GM_xmlhttpRequest({
+                        method: "GET",
+                        url: botLigthUrl + "/off",
+                        onload: function(res) {
+                            window.close();
+                        }
+                    });
+                } else {
+                    window.close();
+                }
             }
         }, 300);
-    }, endDate.getTime() - new Date().getTime());
-    setInterval(function(){
-        if (parseInt($(".wnPUne.N0PJ8e").innerText) < 2) {
-            if (currentState != 0) {
-                console.log("OFF");
-                currentState = 0;
-                GM_xmlhttpRequest({
-                    method: "GET",
-                    url: botLigthUrl + "/off",
-                    onload: function(res) {
-                        currentState = 0;
-                    }
-                });
+    }, timeout);
+    if (botLigthUrl !== "") {
+        setInterval(function(){
+            if (parseInt($(".wnPUne.N0PJ8e")[0].innerText) < 2) {
+                if (currentState != 0) {
+                    console.log("OFF");
+                    currentState = 0;
+                    GM_xmlhttpRequest({
+                        method: "GET",
+                        url: botLigthUrl + "/off",
+                        onload: function(res) {
+                            currentState = 0;
+                        }
+                    });
+                }
+            } else {
+                if (currentState != 1) {
+                    console.log("ON");
+                    currentState = 1;
+                    GM_xmlhttpRequest({
+                        method: "GET",
+                        url: botLigthUrl + "/on",
+                        onload: function(res) {
+                            currentState = 1;
+                        }
+                    });
+                }
             }
-        } else {
-            if (currentState != 1) {
-                console.log("ON");
-                currentState = 1;
-                GM_xmlhttpRequest({
-                    method: "GET",
-                    url: botLigthUrl + "/on",
-                    onload: function(res) {
-                        currentState = 1;
-                    }
-                });
-            }
-        }
-    }, 300);
+        }, 300);
+    }
 });
