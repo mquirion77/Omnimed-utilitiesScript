20c53a92763f2905f7aa9311b8124eb2bc6292a3
Francis <fcorriveau@omnimed.com>
2019-09-24 08:02:46 -0400

    Performance improvement

--- TamperMonkey/development/GithubHD.js
+++ TamperMonkey/development/GithubHD.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       GitHub HD Resolution
 // @namespace
-// @version    2018.10-1
+// @version    2019.09-1
 // @description  Welcome to the year 2000s, HD Resolution is here !
 // @match      https://github.com/*
 // @copyright  2018+, Omnimed.com
@@ -11,23 +11,35 @@
 (function() {
     'use strict';
 
-    var resize = function() {
-      $('.container.new-discussion-timeline.experiment-repo-nav').width('98%');
-      $('.discussion-timeline.pull-discussion-timeline.js-pull-discussion-timeline.js-quote-selection-container.js-review-state-classes').width('80%');
-      $('.container').css('width', '98%');
-      $('.container-lg, .comment-holder').css({
-          'width': '98%',
-          'max-width': '100%'
-      });
-    };
-
     $(document).ready(function() {
-        if ($('.file-wrap').length > 0 || $('.tabnav').length > 0 || $('.repository-content ').length >0) {
-            $(document).bind('DOMSubtreeModified', function() {
-                resize();
-            });
+        if ($('.file-wrap').length > 0 || $('.tabnav').length > 0 || $('.repository-content ').length > 0) {
+            bindResize();
         }
 
         resize();
     });
 })();
+
+function bindResize() {
+    $(document).bind('DOMSubtreeModified', function() {
+        $(document).unbind('DOMSubtreeModified');
+        resize();
+    });
+}
+
+function resize() {
+    try {
+        $('.container.new-discussion-timeline.experiment-repo-nav').width('98%');
+        $('.discussion-timeline.pull-discussion-timeline.js-pull-discussion-timeline.js-quote-selection-container.js-review-state-classes').width('80%');
+        $('.container').css('width', '98%');
+        $('.container-lg, .comment-holder').css({
+            'width': '98%',
+            'max-width': '100%'
+        });
+    } catch (e) {
+    }
+
+    setTimeout(function() {
+        bindResize();
+    }, 500);
+};
