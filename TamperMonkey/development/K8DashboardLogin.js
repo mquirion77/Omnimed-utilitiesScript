9711018c0e168fc4a88dd664615532b62d86c52f
Francis <fcorriveau@omnimed.com>
2019-11-18 08:06:27 -0500

    Kubernetes Dashboard Auto-login.
    Script de login automatique pour K8 Dashboard.

---
+++ TamperMonkey/development/K8DashboardLogin.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         K8 Dashboard Login
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Auto login for Kuberenetes Dashboard
+// @author       You
+// @match        https://k8.omnimed.com/dashboard/*
+// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular.min.js
+// @require      http://malsup.github.io/jquery.blockUI.js
+// @run-at document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    jQuery(function() {
+        if (window.location.href.indexOf('login') !== -1) {
+            var token = 'xxx';
+            $.blockUI({ message: '<h1><img src="https://media.giphy.com/media/yw5wyEXO7RKkE/giphy.gif" /> Kubernetities</h1>' });
+
+            setTimeout(function() {
+                /* Select the token authentication method*/
+                angular.element(document).find('md-radio-button')[1].click();
+                angular.element(document).find('input').focus();
+
+                /* Fill the token values */
+                angular.element(document).find('input').scope().$ctrl.token = token;
+                angular.element(document).find('form')[0][0].value = token;
+                angular.element(document).find('form').scope().$ctrl.loginSpec.token = token;
+
+                /* Trigger the login after Angular update */
+                setTimeout(function() {
+                    angular.element(document).find('form').scope().$ctrl.login();
+                    console.log('logged in');
+                    $.unblockUI();
+                }, 500);
+            }, 1000);
+        }
+    });
+})();
