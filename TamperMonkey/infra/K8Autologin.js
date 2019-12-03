4c30b19acdf8ea22329415c4ca9d967234984151
Francis <fcorriveau@omnimed.com>
2019-12-03 15:26:56 -0500

    COPY function for Token
    - add copy function
    - Remove autologin.

--- TamperMonkey/infra/K8Autologin.js
+++ TamperMonkey/infra/K8Autologin.js
@@ -1,42 +1,39 @@
 // ==UserScript==
 // @name         K8 Dashboard Login
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Auto login for Kuberenetes Dashboard
 // @author       You
 // @maintainer   Philippe Guay
 // @match        https://k8.omnimed.com/*
 // @grant        none
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular.min.js
-// @require      http://malsup.github.io/jquery.blockUI.js
 // @run-at document-end
 // ==/UserScript==
+
+window.copyToClipboard = function(str) {
+    const el = document.createElement('textarea');
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+};
 
 (function() {
     'use strict';
 
     jQuery(function() {
         if (window.location.href.indexOf('login') !== -1) {
-            var token = 'xxx';
-            $.blockUI({ message: '<h1><img src="https://loading.io/spinners/gears/lg.dual-gear-loading-icon.gif" /> Kubernetities</h1>' });
+
+            var token = '!!!!! Philippe Guay is the MAINTAINER OF THIS SCRIPT and WILL update it after every Dashboard rollout !!!!!!';
 
             setTimeout(function() {
                 /* Select the token authentication method*/
-                angular.element(document).find('md-radio-button')[1].click();
-                angular.element(document).find('input').focus();
-
-                /* Fill the token values */
-                angular.element(document).find('input').scope().$ctrl.token = token;
-                angular.element(document).find('form')[0][0].value = token;
-                angular.element(document).find('form').scope().$ctrl.loginSpec.token = token;
-
-                /* Trigger the login after Angular update */
-                setTimeout(function() {
-                    angular.element(document).find('form').scope().$ctrl.login();
-                    console.log('logged in');
-                    $.unblockUI();
-                }, 500);
+                document.getElementsByClassName('mat-radio-label')[1].click()
+                
+                document.getElementsByClassName('kd-login-mode-description')[1].innerHTML =
+                    '<div style="overflow-wrap: break-word;word-wrap: break-word;">' + token + '</div><br /><input type="button" onclick="copyToClipboard(\''+token+ '\');" value="COPY" />';
             }, 1000);
         }
     });
